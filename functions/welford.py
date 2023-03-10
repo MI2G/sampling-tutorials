9958fc12e13af0780eb5749d67f94cdfd9b287de
Teresa Klatzer <teresa.klatzer@gmail.com>
2023-03-10 17:45:19 +0000

    Update welford.py

--- functions/welford.py
+++ functions/welford.py
@@ -1,10 +1,30 @@
-# Welford's algorithm for calculating mean and variance
-# Collection of summary statistics of the Markov chain iterates
+#    Welford's algorithm for calculating mean and variance
+#    Collection of summary statistics of the Markov chain iterates
 # 
-# call update() when generating a new sample
-# call get_mean() or get_var() to obtain the current mean or variance
+#    call update() when generating a new sample
+#    call get_mean() or get_var() to obtain the current mean or variance
+#
+#    Copyright (C) 2023 MI2G
+#    Dobson, Paul pdobson@ed.ac.uk
+#    Kemajou, Mbakam Charlesquin cmk2000@hw.ac.uk
+#    Klatzer, Teresa t.klatzer@sms.ed.ac.uk
+#    Melidonis, Savvas sm2041@hw.ac.uk
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 # https://doi.org/10.2307/1266577
+
 class welford: 
     def __init__(self, x):
         self.k = 1
