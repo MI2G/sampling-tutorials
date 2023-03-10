504c6285a76845ff933b22f211b293ed3914dd33
Teresa Klatzer <teresa.klatzer@gmail.com>
2023-03-10 17:39:06 +0000

    Update measures.py

--- functions/measures.py
+++ functions/measures.py
@@ -1,3 +1,26 @@
+#    Wrapper functions for computing the normalized root mean 
+#    square error (NRMSE), structural similarity index (SSIM),
+#    peak signal to noise ratio (PSNR).
+#
+#    Copyright (C) 2023 MI2G
+#    Paul Dobson pdobson@ed.ac.uk
+#    Charlesquin Kemajou cmk2000@hw.ac.uk
+#    Teresa Klatzer t.klatzer@sms.ed.ac.uk
+#    Savvas Melidonis sm2041@hw.ac.uk
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import peak_signal_noise_ratio as psnr
 import numpy as np
