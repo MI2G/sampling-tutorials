c825e868a1cc6ad01e211df63306dadd217bdf14
Teresa Klatzer <teresa.klatzer@gmail.com>
2023-04-07 12:53:50 +0100

    Create info.py

---
+++ info.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+"""
+This file contains parameters to fill settings in setup.py for 
+the sampling-tools package.
+"""
+
+__classifiers__ = ["Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License v3.0",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Windows",      
+        "Topic :: Scientific/Engineering",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers"],
+
+__about__  = ("Tools for sampling tutorials")
+
+__long_description__ = """
+==========
+MI2G sampling-tutorials
+==========
+Imaging Inverse Problems and Bayesian Computation - Python tutorials to learn 
+about (accelerated) sampling for uncertainty quantification and other advanced inferences.
+Licensed under the terms of the GNU General Public License v3.0.
+Information can be found on the Github main repository,
+https://github.com/MI2G/sampling-tutorials
+"""
+
+# Main setup parameters
+__name__                = 'sampling-tutorials'
+__maintainer__          = "MI2G group: S. Melidonis, T. Klatzer, P. Dobson, C. Kemajou"
+__maintainer_email__    = "t.klatzer@sms.ed.ac.uk"
+__url__                 = "https://github.com/MI2G/sampling-tutorials"
+__download_url__        = "https://github.com/MI2G/sampling-tutorials"
+__license__             = "GNU General Public License v3.0"
+__author__              = "MI2G group: S. Melidonis, T. Klatzer, P. Dobson, C. Kemajou"
+__author_email__        = "t.klatzer@sms.ed.ac.uk"
+__platforms__           = "Linux, Windows, MACOSX"
+__version__             = "0.0.1"
+__provides__            = ["sampling_tools"]
