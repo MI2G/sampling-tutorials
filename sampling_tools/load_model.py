256108e10c46ddb147a752f62245ccbc8e34fa91
Teresa Klatzer <teresa.klatzer@gmail.com>
2023-06-26 16:16:58 +0100

    Update load_model.py

--- sampling_tools/load_model.py
+++ sampling_tools/load_model.py
@@ -1,3 +1,31 @@
+#    Loads the neural network to be used within the Pnp-ULA algorithm
+
+#    The network and source code was developed by the authors of this paper
+#    E. K. Ryu, J. Liu, S. Wang, X. Chen, Z. Wang, and W. Yin. 
+#    "Plug-and-Play Methods Provably Converge with Properly Trained Denoisers." ICML, 2019.
+
+#    GitHub account https://github.com/uclaopt/Provable_Plug_and_Play/
+
+#    (minorly) adapted in pytorch by: MI2G
+#    Copyright (C) 2023 MI2G
+#    Dobson, Paul pdobson@ed.ac.uk
+#    Kemajou, Mbakam Charlesquin cmk2000@hw.ac.uk
+#    Klatzer, Teresa t.klatzer@sms.ed.ac.uk
+#    Melidonis, Savvas sm2041@hw.ac.uk
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import torch
 import torch.nn as nn
 import numpy as np
